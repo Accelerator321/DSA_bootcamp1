370fab4c76eeeecbd4f9e1bd6a40feb5bf5333a6
Shyam Mohan Gupta <shyamprime2610@gmail.com>
2023-01-04 00:26:08 +0530

    Add files via upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,58 @@
+import styles from "../style";
+import { discount, image} from "../assets";
+// import dsa from "../assets/dsa.jpg";
+import GetStarted from "./GetStarted";
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">Join us</span> in this{" "}
+            <span className="text-white">Amazing</span> Bootcamp
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            DSA <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Bootcamp</span>{" "}
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
+        </div>
+
+        {/* <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Payment Method.
+        </h1> */}
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          This Bootcamp will boost your DSA Journey.
+          <br />
+          Are u a beginer? 
+          <br />
+          Don't worry this Bootcamp will be the start of your Dsa journey.
+
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={image} style={{width:'60%',height:'60%'}} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
